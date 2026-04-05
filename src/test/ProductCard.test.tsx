fe34896d0cbd9230f5a722fe17cba7cc302fadf9
mgenius3 <benmos16@gmail.com>
2026-04-05 08:20:07 +0100

    updated exported rendering issue on nextjs

--- src/test/ProductCard.test.tsx
+++ src/test/ProductCard.test.tsx
@@ -1,5 +1,4 @@
-import { render } from "@testing-library/react";
-import { screen } from "@testing-library/dom";
+import { render, screen } from "@testing-library/react";
 import { ProductCard } from "../components/ProductCard";
 import { Product } from "../types/product";
 
